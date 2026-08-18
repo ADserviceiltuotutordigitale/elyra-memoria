e411e0803b38e8f44ee503b204f470d051a3027e
ADserviceiltuotutordigitale <info@ad-service.org>
2026-08-18 19:19:04 +0200

    Rimuove la vecchia board dei task personali e le rotte CRM ormai inutilizzate

--- app/api/crm/task/[id]/completa/route.js
+++
@@ -1,7 +0,0 @@
-import { completeTask } from "@/lib/store";
-
-export async function POST(request, { params }) {
-  const { id } = await params;
-  const task = await completeTask(id);
-  return Response.json({ task });
-}
